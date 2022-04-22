7dfc620836c7caaa7db129be6f34585b2b3ee2e2
Mershana <102474997+Mershana@users.noreply.github.com>
2022-04-22 19:32:06 +0300

    basics_js

---
+++ 34_вывод в документ/js/app.js
@@ -0,0 +1,38 @@
+// Задача 1
+// Выведите в окно браузера какой-нибудь текст.
+
+
+'use stict'
+
+// document.write('Hello World')
+
+// Задача 2
+// Выведите в окно браузера какой-нибудь текст, сделав его курсивным.
+// ВАРИАНТ 1
+// document.write('<i>Hello World</i>')
+// ВАРИАНТ 2
+// let text = ('<i>hello world</i>');
+// document.write(text)
+// ВАРИАНТ 3
+// let text = ('hello world')
+// document.write('<i>' + text + '</i>')
+
+// Задача 3
+// Дана переменная:
+// let str = 'text';
+// Выведите текст из переменной в окно браузера так, чтобы он был курсивным.
+// let str = 'text';
+// document.write('<i>' + str + '</i>')
+
+// Задача 4
+// Выведите в окно браузера столбец чисел от 1 до 5.
+document.write('1');
+document.write('<br>');
+document.write('2');
+document.write('<br>');
+document.write('3');
+document.write('<br>');
+document.write('4');
+document.write('<br>');
+document.write('5');
+document.write('<br>');
